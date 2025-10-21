9d299a9e7a16eb4eff22085a0b832603f51b0667
Justin <dukk@dukk.org>
2025-10-20 21:48:15 -0400

    Updating scheduler scripts and template to fix issues

--- scheduler/container/scripts/docker-entrypoint.d/20-ensure-crontab-setup.sh
+++ scheduler/container/scripts/docker-entrypoint.d/20-ensure-crontab-setup.sh
@@ -5,8 +5,8 @@ crontab -u quackjob -l > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "No crontab found for quackjob user; creating default crontab"
     echo "#*0 3 1 * * /usr/bin/quackjob --job=\"build-image-file-list.json\" >> /quackview/logs/cron-run.log 2>&1" | crontab -u quackjob -
-    echo "#*0 /1 * * * /usr/bin/quackjob --job=\"calendar-events.json\" >> /quackview/logs/cron-run.log 2>&1" | crontab -u quackjob -
-    echo "#0 4 /7 * * /usr/bin/quackjob --job=\"dad-jokes.json\" >> /quackview/logs/cron-run.log 2>&1" | crontab -u quackjob -
+    (crontab -l; echo "#*0 /1 * * * /usr/bin/quackjob --job=\"calendar-events.json\" >> /quackview/logs/cron-run.log 2>&1") | crontab -u quackjob -
+    (crontab -l; echo "#0 4 /7 * * /usr/bin/quackjob --job=\"dad-jokes.json\" >> /quackview/logs/cron-run.log 2>&1") | crontab -u quackjob -
     ln -sf /etc/crontabs/quackjob /quackview/config/crontab
 else
     echo "Crontab already set up for quackjob user; skipping"
