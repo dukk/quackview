ed375d4522696c4cb3cc8ce73682cf7922073d96
Justin <dukk@dukk.org>
2025-10-24 23:15:04 -0400

    Fixing script error

--- scheduler/container/scripts/docker-entrypoint.d/20-ensure-crontab-setup.sh
+++ scheduler/container/scripts/docker-entrypoint.d/20-ensure-crontab-setup.sh
@@ -6,7 +6,7 @@ if [ $? -ne 0 ]; then
     echo "No crontab found for quackjob user; creating default crontab"
     echo "#*0 3 1 * * /usr/bin/quackjob --job=\"build-image-file-list.json\" >> /quackview/logs/cron-run.log 2>&1" | crontab -u quackjob -
     (crontab -l; echo "#*0 /1 * * * /usr/bin/quackjob --job=\"calendar-events.json\" >> /quackview/logs/cron-run.log 2>&1") | crontab -u quackjob -
-    (crontab -l; echo "#0 4 /7 * * /usr/bin/quackjob --job=\"dad-jokes.json\" >> /quackview/logs/cron-run.log 2>&1" | crontab -u quackjob -
+    (crontab -l; echo "#0 4 /7 * * /usr/bin/quackjob --job=\"dad-jokes.json\" >> /quackview/logs/cron-run.log 2>&1") | crontab -u quackjob -
     ln -sf /etc/crontabs/quackjob /quackview/config/crontab
 else
     echo "Crontab already set up for quackjob user; skipping"
