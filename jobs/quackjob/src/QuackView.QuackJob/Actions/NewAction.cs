a073846cb394afc71ae99e57cfd31face3dfc61c
Justin <dukk@dukk.org>
2025-10-26 16:56:47 -0400

    Adding security and disabling template copy temporarily

--- jobs/quackjob/src/QuackView.QuackJob/Actions/NewAction.cs
+++ jobs/quackjob/src/QuackView.QuackJob/Actions/NewAction.cs
@@ -1,21 +1,25 @@
-using System.Reflection;
-using System.Text.RegularExpressions;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using TypoDukk.QuackView.QuackJob.Jobs;
-using TypoDukk.QuackView.QuackJob.Services;
+// using System.Reflection;
+// using System.Text.RegularExpressions;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.Logging;
+// using TypoDukk.QuackView.QuackJob.Jobs;
+// using TypoDukk.QuackView.QuackJob.Services;
 
-namespace TypoDukk.QuackView.QuackJob.Actions;
+// namespace TypoDukk.QuackView.QuackJob.Actions;
 
-// TODO: Come back to this after I have a job management service implemented.
-
-// internal class NewAction(ILogger<NewAction> logger, IConsoleService console, IServiceProvider serviceProvider) : Action(console)
+// internal class NewAction(
+//     ILogger<NewAction> logger,
+//     IConsoleService console,
+//     IServiceProvider serviceProvider,
+//     IJobManagementService jobManagement)
+//     : Action(logger, console)
 // {
-//     private readonly ILogger<NewAction> logger = logger ?? throw new ArgumentNullException(nameof(logger));
-//     private readonly IServiceProvider serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+//     protected readonly ILogger<NewAction> Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+//     protected readonly IServiceProvider ServiceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+//     protected readonly IJobManagementService JobManagement = jobManagement ?? throw new ArgumentNullException(nameof(jobManagement));
 
 //     public override async Task ExecuteAsync(string[] args)
 //     {
-        
+
 //     }
 // }
--- scheduler/container/scripts/docker-entrypoint.d/10-volumn-template-setup.sh
+++ scheduler/container/scripts/docker-entrypoint.d/10-volumn-template-setup.sh
@@ -4,8 +4,8 @@
 # mkdir -p /quackview/config/ /quackview/jobs/ /quackview/data/ /quackview/secrets/ /quackview/logs/
 
 if [ -d /quackview-template ]; then
-    echo "Copying template files to /quackview"
-    rsync -av --backup --suffix=".new" /quackview-template/ /quackview
+    #echo "Copying template files to /quackview"
+    #rsync -av --backup --suffix=".new" /quackview-template/ /quackview
     #cp -rnv /quackview-template/* /quackview
 else
     echo "No /quackview-template directory found; skipping template copy"
@@ -18,3 +18,11 @@ fi
 
 echo "Setting ownership of /quackview/ to quackjob user"
 chown -R quackjob:quackjob /quackview/
+
+if [ -d /quackview/secrets ]; then
+    echo "Hardening permissions on /quackview/secrets"
+    chmod -R ug+rwX,o-rwx /quackview/secrets
+    find /quackview/secrets -type d -exec chmod g+s {} \;
+else
+    echo "No /quackview/secrets directory found; skipping permission hardening"
+fi
